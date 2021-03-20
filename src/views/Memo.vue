271b19b75f4608060745aed624ebfb4788d8da7a
Sandsunset <sejoon0606@gmail.com>
2021-03-20 17:35:51 +0900

    refactor: Memo.vue

--- src/views/Memo.vue
+++ src/views/Memo.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="memo">
-    <ul class="memos"></ul>
+    <ul class="memos">
+      <li v-for="Memo in memos" v-bind:key="Memo">
+        <div class="box">
+          <div class="title">
+            {{ Memo.title }}
+          </div>
+          <div class="memo">
+            {{ Memo.memo }}
+          </div>
+        </div>
+      </li>
+    </ul>
     <img id="plus" @click="hello" src="@/assets/plus.png" height=35px width=35px>
   </div>
 </template>
 
 <script>
 const memos = document.getElementsByClassName("memos");
+
 export default {
   name: "Memo",
+  data() {
+    return {
+      memos: [
+        {title:'title',memo:'memo'},
+        {title:'title2',memo:'이건메모2'}
+      ]
+    }
+  },
   methods: {
     hello() {
       var newBox = document.createElement("li");
-      newBox.innerHTML = '<div class="box"><p>asdf</p></div><style>.box {width:200px;height:300px;border: 1px solid;border-color: black;border-radius: 10px;margin: 5px}li{float:left;}</style>'
+      newBox.innerHTML = '<div class="box"><p>asdf</p></div>'
       memos[0].appendChild(newBox);
     }
   }
@@ -24,6 +44,8 @@ export default {
   margin: 1%;
   float: left;
 }
+.box {width:200px;height:300px;border: 1px solid;border-color: black;border-radius: 10px;margin: 5px}
+li {float:left;}
 ul {
   list-style: none;
   padding: 0;
